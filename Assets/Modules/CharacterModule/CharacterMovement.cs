1dcdd88a8d79b0f0ee9dfc1cd78121a1e0e98c6f
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2022-11-19 19:32:13 -0300

    Init player controller and character movement

---
+++ Assets/Modules/CharacterModule/CharacterMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AIProject.CharacterModule
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+
+
+        // Unity Methods -----------------------------------
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+
+        // Public Methods -----------------------------------
+        
+    }
+}
---
+++ Assets/Modules/CharacterModule/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AIProject.CharacterModule
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Unity Methods -------------------------------------
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+
+        // Private Methods ------------------------------------
+    }
+}
